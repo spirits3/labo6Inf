b2e728e3b055d9ff6d0abad3c36205c441c4b65b
Jorge Esteves <jorge.fulgencioesteves@heig-vd.ch>
2018-05-30 14:14:28 +0200

    bateau.h first draft

--- bateau.h
+++ bateau.h
@@ -1,5 +1,36 @@
 #ifndef BATEAU_H
 #define BATEAU_H
 
+enum typeBateau { VOILIER, MOTEUR };
+enum typeBateauAMoteur {PECHE, PLAISANCE};
 
+struct Bateau {
+    const char* nom;
+    typeBateau typeA;
+    union {
+        struct Voilier voilier;
+        struct BateauAMoteur bateauMoteur;
+    };
+};
+
+struct Voilier {
+    uint16_t surfaceVoilure;
+};
+
+struct BateauAMoteur {
+    uint16_t puissance;
+    typeBateauAMoteur typeB;
+    union {
+        struct BateauPeche;
+        struct BateauPlaisance;
+    };
+};
+
+struct BateauPeche {
+    uint16_t tonnePoisson;
+};
+
+struct BateauPlaisance {
+    const char* nomProprietaire;
+};
 #endif
