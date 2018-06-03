8267f4094708f0b53f84beeaefe8ae2337d22f32
Rafiibro <39766039+Rafiibro@users.noreply.github.com>
2018-06-03 21:37:47 +0200

    Update taxe.c

--- taxe.c
+++ taxe.c
@@ -18,39 +18,55 @@
 #include "taxe.h"
 
 double taxeBase (const Bateau* bateau) {
-    return bateau->typeA ? TAXE_BASE_VOILIER : TAXE_BASE_MOTEUR;
+    return bateau->typeA ? TAXE_BASE_MOTEUR : TAXE_BASE_VOILIER;
 }
 
 double taxeSpecifique(const Bateau* bateau) {
 	
 	if (bateau->typeA == VOILIER) {
-		return bateau->voilier.surfaceVoilure < SURFACE_VOILE_MAX_AVANT_TAXE ?
-			   TAXE_SPECIFIQUE_PETITE_VOILE : TAXE_SPECIFIQUE_GRANDE_VOILE;
+		return bateau->voilier.surfaceVoilure < SURFACE_VOILE_MAX_AVANT_TAXE ? TAXE_SPECIFIQUE_PETITE_VOILE : TAXE_SPECIFIQUE_GRANDE_VOILE;
 	}
 
 	if (bateau->bateauMoteur.typeB == PECHE) {
-		return bateau->bateauMoteur.bateauPeche.tonnePoisson < TONNAGE_MAX_AVANT_TAXE ?
-			   TAXE_SPECIFIQUE_PETIT_TONNAGE : TAXE_SPECIFIQUE_GRAND_TONNAGE;
+		return bateau->bateauMoteur.bateauPeche.tonnePoisson < TONNAGE_MAX_AVANT_TAXE ? TAXE_SPECIFIQUE_PETIT_TONNAGE : TAXE_SPECIFIQUE_GRAND_TONNAGE;
 	}
 
 	if (bateau->bateauMoteur.typeB == PLAISANCE) {
-		return bateau->bateauMoteur.puissance < PUISSANCE_MAX_AVANT_TAXE ?
-			   TAXE_SPECIFIQUE_PETIT_MOTEUR : TAXE_SPECIFIQUE_GRAND_MOTEUR;
+		return bateau->bateauMoteur.puissance < PUISSANCE_MAX_AVANT_TAXE ? TAXE_SPECIFIQUE_PETIT_MOTEUR : TAXE_SPECIFIQUE_GRAND_MOTEUR;
 	}
 	return 0;
 }
 
 double taxeParBateau(const Bateau* bateau) {
-	return taxeBase(bateau) + taxeSpecifique(bateau);
+    return taxeBase(bateau) + taxeSpecifique(bateau);
 }
 
-void taxeTotalPort(Bateau port[], double *taxeParType,uint8_t nombreDeBateau) {
+void taxeTotalPort(Bateau port[], double taxeParType[],uint8_t nombreDeBateau) {
 	for(size_t i = 0; i < nombreDeBateau; ++i) {
-	    if (port[i].typeA == VOILIER) 
-			taxeParType[0] += taxeParBateau(&port[i]);
-	    else if (port[i].bateauMoteur.typeB == PECHE) 
-			taxeParType[1] += taxeParBateau(&port[i]);
-	    else if (port[i].bateauMoteur.typeB == PLAISANCE) 
-			taxeParType[2] += taxeParBateau(&port[i]);
+            if (port[i].typeA == VOILIER) 
+                taxeParType[0] += taxeParBateau(&port[i]);
+            else if (port[i].bateauMoteur.typeB == PECHE) 
+                taxeParType[1] += taxeParBateau(&port[i]);
+            else if (port[i].bateauMoteur.typeB == PLAISANCE) 
+                taxeParType[2] += taxeParBateau(&port[i]);
 	}
+}
+
+void taxeMoyennePort(Bateau port[], double taxeMoyenneType[], uint8_t nombreDeBateau) {
+    
+    uint8_t *nombre_bateau_type = (uint8_t*)calloc(NOMBRE_TYPE_BATEAU, sizeof(uint8_t));
+
+    for(size_t i = 0; i < nombreDeBateau; ++i) {
+        if (port[i].typeA == VOILIER) 
+            nombre_bateau_type[0]++;
+        else if (port[i].bateauMoteur.typeB == PECHE) 
+            nombre_bateau_type[1]++;
+        else if (port[i].bateauMoteur.typeB == PLAISANCE) 
+            nombre_bateau_type[2]++;
+    }
+    
+    taxeTotalPort(port, taxeMoyenneType, nombreDeBateau);
+    
+    for(size_t i = 0; i < NOMBRE_TYPE_BATEAU; ++i) 
+        taxeMoyenneType[i] /= nombre_bateau_type[i];   
 }
